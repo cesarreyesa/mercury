c1d63f3bffef806e60b68774104288028f7d60b3
Cesar Reyes <cesar@nopalsoft.mx>
2010-11-30 22:09:27 -0600

    Agrupacion de incidencias

---
+++ mercury-server/src/groovy/org/nopalsoft/mercury/domain/IssueFilter.groovy
@@ -0,0 +1,24 @@
+package org.nopalsoft.mercury.domain
+
+/**
+ * User: cesarreyes
+ * Date: 30/11/10
+ * Time: 21:29
+ */
+class IssueFilter{
+  int id
+  String status
+  String priority
+  String assignee
+  String from
+  String name
+  String reporter
+  GroupBy groupBy
+}
+
+enum GroupBy {
+  Priority,
+  Status,
+  Assignee,
+  Reporter
+}
