7b554a0fbc12b5615d93b1dd49072a8d6e7a1515
Cesar Reyes <cesar@nopalsoft.mx>
2011-05-30 22:51:38 -0500

    Avances para interface mobile

--- mercury-server/grails-app/conf/BootStrap.groovy
+++ mercury-server/grails-app/conf/BootStrap.groovy
@@ -1,49 +1,35 @@
 import org.nopalsoft.mercury.domain.User
 import org.nopalsoft.mercury.domain.Role
+import org.codehaus.groovy.grails.commons.ApplicationHolder
 
 class BootStrap {
 
-  def springSecurityService
+   def springSecurityService
 
-  def init = {servletContext ->
-    String.metaClass.'static'.randomString = { length ->
-      // The chars used for the random string
-      def list = ('a'..'z')+('A'..'Z')+('0'..'9')
-      // Make sure the list is long enough
-      list = list * (1+length / list.size())
-      // Shuffle it up good
-      Collections.shuffle(list)
-      length > 0 ? list[0..length-1].join() : ''
-    }
-    def admin = User.findByUsername("cesar.reyes")
-//    if (admin) {
-//      if (!admin.authorities) {
-//        def role = new Role(authority: "ROLE_ADMIN", description: "Administrator")
-//        role.save()
-//        admin.addToAuthorities role
-//        if (admin.save()) {
-//          println "Updated admin user: cesar.reyes"
-//        } else {
-//          pringln "Admin user cesar.reyes not updated: ${admin.errors}"
-//        }
-//      }
-//    }else {
-//      admin = new User()
-//      admin.username = "cesar.reyes"
-//      admin.password = authenticateService.encodePassword("21Chivas")
-////      admin.userRealName = "Cesar Reyes"
-//      admin.email = "cesar@mexicovenuefinders.com"
-//      admin.enabled = true
-//      def role = new Role(authority: "ROLE_ADMIN", description: "Administrator")
-//      role.save()
-//      admin.addToAuthorities role
-//      if (admin.save()) {
-//        println "Created admin user: cesar.reyes"
-//      } else {
-//        pringln "Admin user cesar.reyes not created: ${admin.errors}"
-//      }
-//    }
-  }
-  def destroy = {
-  }
+   def init = {servletContext ->
+      String.metaClass.'static'.randomString = { length ->
+         // The chars used for the random string
+         def list = ('a'..'z') + ('A'..'Z') + ('0'..'9')
+         // Make sure the list is long enough
+         list = list * (1 + length / list.size())
+         // Shuffle it up good
+         Collections.shuffle(list)
+         length > 0 ? list[0..length - 1].join() : ''
+
+      }
+
+      def application = ApplicationHolder.application
+      application.getArtefacts("Controller").each { klass -> addDynamicMethods(klass) }
+   }
+
+   private addDynamicMethods(klass) {
+
+      klass.metaClass.isMobileDevice = {
+         def device = request.getAttribute("currentDevice")
+         device.isMobile()
+      }
+   }
+
+   def destroy = {
+   }
 }
